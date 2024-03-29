ef974f0efd2bed44f6bc1500e3401f8b071f7d46
Alex Nasulloev <nasulloevalex@icloud.com>
2024-03-29 21:47:47 +0500

    Initial Commit

---
+++ ShowchatMyFollowers/ShowchatMyFollowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowchatMyFollowersApp.swift
+//  ShowchatMyFollowers
+//
+//  Created by Alex Nasulloev on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ShowchatMyFollowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
